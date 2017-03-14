24aee83e5060265988fd2825126dfdd70f20c07f
Zeeshan Ahmad <zeeshan.ahmad@devsinc.com>
2017-03-14 19:25:55 +0500

    First commit

---
+++ config/routes.rb
@@ -0,0 +1,6 @@
+Rails.application.routes.draw do
+  scope '/api' do
+    resources :groups, except: [:new, :edit]
+  end
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
